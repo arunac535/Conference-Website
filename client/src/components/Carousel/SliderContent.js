7631af71f4e949ff94f3ccc2006d57754392924f
Aakash Mallik <121702227+mallikaakash@users.noreply.github.com>
2023-12-23 10:18:07 +0530

    changes in carousel

--- client/src/components/Carousel/SliderContent.js
+++ client/src/components/Carousel/SliderContent.js
@@ -10,6 +10,7 @@ export default function SliderContent({activeIndex,imageSlider}) {
             <img className="slide-image" src={slide.urls} alt=""/>
             <h2 className="slide-title">International Conference on Advancements in Smart Sustainable Energy Sources, Technologies and Systems</h2> 
             <h3 className="slide-sub-title"> March 11th-12th, 2024</h3>  
+            <h3 className="slide-sub-title"> National Institute of Technology Karnataka, Surathkal</h3>  
         </div>
         ))}
     </section>
