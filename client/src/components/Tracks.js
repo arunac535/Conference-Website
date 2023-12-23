bf25e9584c6365154bca0f72d421beb8c3264d04
Aditya <adityachauhan2501@gmail.com>
2023-12-23 12:12:59 +0530

    update tracks

--- client/src/components/Tracks.js
+++ client/src/components/Tracks.js
@@ -22,23 +22,23 @@ export default function Tracks() {
             <span className="font-bold text-lg">Transportation</span>
           </div>
           <div className="p-4 text-sm rounded-lg bg-white text-gray-900 shadow-md">
-            <span className="font-medium">TT3:</span><br />
+            <span className="font-medium">TT4:</span><br />
             <span className="font-bold text-lg">Control and Automation</span>
           </div>
           <div className="p-4 text-sm rounded-lg bg-white text-gray-900 shadow-md">
-            <span className="font-medium">TT3:</span><br />
+            <span className="font-medium">TT5:</span><br />
             <span className="font-bold text-lg">Renewable Energy Systems and Energy Storage</span>
           </div>
           <div className="p-4 text-sm rounded-lg bg-white text-gray-900 shadow-md">
-            <span className="font-medium">TT3:</span><br />
+            <span className="font-medium">TT6:</span><br />
             <span className="font-bold text-lg">Smart Grids & Power Quality</span>
           </div>
           <div className="p-4 text-sm rounded-lg bg-white text-gray-900 shadow-md">
-            <span className="font-medium">TT3:</span><br />
+            <span className="font-medium">TT7:</span><br />
             <span className="font-bold text-lg">Power Engineering Educations and Issues</span>
           </div>
           <div className="p-4 text-sm rounded-lg bg-white text-gray-900 shadow-md">
-            <span className="font-medium">TT3:</span><br />
+            <span className="font-medium">TT8:</span><br />
             <span className="font-bold text-lg">Applications of AI and ML Techniques to
 					Power Electronics</span>
           </div>
