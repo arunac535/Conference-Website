886f839b0788277c8d28be343094cf60e14926b0
Aditya <adityachauhan2501@gmail.com>
2023-12-23 21:08:07 +0530

    msg

--- client/src/components/CarouselWithText.js
+++ client/src/components/CarouselWithText.js
@@ -2,8 +2,8 @@ import { Carousel, Typography, Button } from "@material-tailwind/react";
  
 export default function CarouselWithContent() {
   return (
-    <Carousel className="">
-      <div className="h-50 w-full">
+    <Carousel >
+      <div className="relative h-full w-full">
         <img
           src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
           alt="image 1"
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,35 +1,31 @@
-import React from 'react';
+import React from "react";
 
-import Button from '../components/Button';
-import Main from '../components/Main';
-import ActivitiesTimeline from '../components/ActivitiesTimeline';
-import Footer from '../components/Footer';
-import Tracks from '../components/Tracks';  
-import CarouselWithContent from '../components/CarouselWithText';
-
+import Button from "../components/Button";
+import Main from "../components/Main";
+import ActivitiesTimeline from "../components/ActivitiesTimeline";
+import Footer from "../components/Footer";
+import Tracks from "../components/Tracks";
+import CarouselWithContent from "../components/CarouselWithText";
 
 export default function Home() {
   return (
     <>
-        <header>
-            {/* <Carousel /> */}
-            <CarouselWithContent />
-            <Button text="Brochure" link="" />
-        </header>
-        <section className='container mx-auto flex flex-col items-center justify-center'>
+      <header>
+        {/* <Carousel /> */}
 
-            <Main />
-            <Tracks />
-            <ActivitiesTimeline />
+        <CarouselWithContent />
 
-        </section>
+        <Button text="Brochure" link="" />
+      </header>
+      <section className="container mx-auto flex flex-col items-center justify-center">
+        <Main />
+        <Tracks />
+        <ActivitiesTimeline />
+      </section>
 
-        <footer>
-            <Footer />
-        </footer>
-
-
-        
+      <footer>
+        <Footer />
+      </footer>
     </>
-  )
+  );
 }
