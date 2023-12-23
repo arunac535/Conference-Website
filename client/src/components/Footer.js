b066141b5d16c165bf76108e2762b02993ec69a3
Aditya <adityachauhan2501@gmail.com>
2023-12-23 12:27:52 +0530

    updated footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="bg-white w-full rounded-lg dark:bg-gray-800">
+      <div className="mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          &copy; 2023-2024
+          <Link to ="" className="hover:underline">
+            PEDES
+          </Link>
+          . All Rights Reserved.
+        </span>
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+          <li>
+            <Link to ="/about" className="mr-4 hover:underline md:mr-6">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to ="/contact" className="hover:underline">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 }
