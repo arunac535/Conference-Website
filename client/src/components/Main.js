bd0c6fefa5c3c445be6fb4aef169449d562f1fdd
Aditya <adityachauhan2501@gmail.com>
2023-12-23 10:21:51 +0530

    update-main

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -2,6 +2,27 @@ import React from 'react'
 
 export default function Main() {
   return (
-    <div>Main</div>
+    <div>
+      <h1>About PEDES</h1>
+      <p>
+        IEEE Power Electronics, Drives and Energy Systems (PEDES) is a biennial conference sponsored by IEEE Industry Applications Society and technically co-sponsored by IEEE Power Electronics Society, Industrial Electronics Society, and Power & Energy Society. The upcoming IEEE PEDES 2024 conference will be held at National Institute of Technology Karnataka (NITK) Surathkal, Mangaluru from 18th - 21st December 2024.
+      </p>
+      <p>
+        The goal of PEDES is to provide a platform for conference, exhibition, and tutorial on selected topics of contemporary interest in power electronics, drives, and energy system subjects, especially in relation to energy affected by climate change.
+      </p>
+      <p>
+        NITK, a premier institution recognized as an institute of national importance, will host the conference. The Department of Electrical and Electronics Engineering at NITK is committed to excellence in education, training, and research, offering various programs and actively engaging in cutting-edge research projects and consultancy projects.
+      </p>
+
+      <h2>About NITK</h2>
+      <p>
+        National Institute of Technology Karnataka, Surathkal is a premier Institution engaged in imparting quality technological education and providing support to research and development activities. NITK is recognized as an institute of national importance by an act of parliament. NITK has carved a niche for itself among the best technical institutes in India. NITK offers 9 UG, 26 PG programs and PhD programs.
+      </p>
+
+      <h2>Department of Electrical & Electronics Engineering</h2>
+      <p>
+        The Department of Electrical and Electronics Engineering at NITK strives to be a center of excellence in education, training, and research. The faculty is involved in several research projects in cutting-edge technologies and publications in many international journals and conferences. Additionally, the department undertakes many consultancy projects from industry and other organizations.
+      </p>
+    </div>
   )
 }
