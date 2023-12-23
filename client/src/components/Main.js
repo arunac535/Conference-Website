c92fb7a6a743541246d7d470104f2bc3b7d18175
Aditya <adityachauhan2501@gmail.com>
2023-12-23 19:54:50 +0530

    images updated

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -8,15 +8,6 @@ export default function Main() {
       
       <Heading text="Society Sponsors"/>
       <Logo2 />
-      <h1 className="text-5xl font-bold m-10 text-center font-helvetica">About PEDES</h1>
-      <p className="text-lg mb-4 text-gray-800 text-center">
-        IEEE Power Electronics, Drives and Energy Systems (PEDES) is a biennial
-        conference sponsored by IEEE Industry Applications Society and
-        technically co-sponsored by IEEE Power Electronics Society, Industrial
-        Electronics Society, and Power & Energy Society. The upcoming IEEE PEDES
-        2024 conference will be held at National Institute of Technology
-        Karnataka (NITK) Surathkal, Mangaluru from 18th - 21st December 2024.
-      </p>
     </div>
   );
 }
