624e07b9f1a1f68e5dcf3dab7dffe2853935901a
Aditya <adityachauhan2501@gmail.com>
2023-12-24 21:47:01 +0530

    routing-updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
 import Accomodation from "./pages/Accomodation";
@@ -10,23 +10,30 @@ import ForAuthors from "./pages/ForAuthors";
 import Registration from "./pages/Registration";
 import Speakers from "./pages/Speakers";
 import Sponsorship from "./pages/Sponsorship";
-
+import TracksList from "./components/TracksList";
+import CallForPapers from "./components/CallForPapers";
+import PaperSubmission from "./components/PaperSubmission";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" index element={<Home />} />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/ForAuthors" element={<ForAuthors />} />
-          <Route path="/Commitee" element={<Committee />} />
-          <Route path="/Speakers" element={<Speakers />} />
-          <Route path="/EventDetails" element={<EventDetails />} />
-          <Route path="/Registration" element={<Registration />} />
-          <Route path="/Sponsorship" element={<Sponsorship />} />
-          <Route path="/Accomodation" element={<Accomodation />} />
-          <Route path="/Contact" element={<Contact />} />
+          <Route index element={<Home />} />
+          <Route path="about" element={<AboutUs />} />
+          <Route path="authors" element={<ForAuthors />}>
+            <Route index element={<Navigate replace to="tracks" />} />
+            <Route path="tracks" element={<TracksList />} />
+            <Route path="callforpapers" element={<CallForPapers />} />
+            <Route path="papersubmission" element={<PaperSubmission />} />
+          </Route>
+          <Route path="commitee" element={<Committee />} />
+          <Route path="speakers" element={<Speakers />} />
+          <Route path="eventDetails" element={<EventDetails />} />
+          <Route path="registration" element={<Registration />} />
+          <Route path="sponsorship" element={<Sponsorship />} />
+          <Route path="accomodation" element={<Accomodation />} />
+          <Route path="contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
     </>
--- client/src/pages/ForAuthors.js
+++ client/src/pages/ForAuthors.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import Logo from '../components/Logo'
 import Navbar from '../components/Navbar'
+import { Outlet } from 'react-router-dom'
+import Footer from '../components/Footer'
 
 function ForAuthors() {
   return (
+    <>
+
     <header>
       <Logo />
       <Navbar />
     </header>
+    <main>
+      <Outlet />
+    </main>
+    <footer>
+      <Footer />
+    </footer>
+    </>
   )
 }
 
