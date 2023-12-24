15ea7a3491bba2ae09b908665a17ed90a2579c7b
arunac535 <arunac535@gmail.com>
2023-12-24 11:39:11 +0530

    add

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -8,7 +8,7 @@ export default function Button({ text, link }) {
   return (
     <div className="flex justify-center">
       <button
-        className="bg-gray-800 p-4 hover:bg-gray-700  text-white text-2xl font-bold py-3 rounded-lg w-50 shadow-md transition duration-300 ease-in-out"
+        className="h-10 px-5 m-2 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800"
         onclick={handleRedirect}
       >
         {text}
