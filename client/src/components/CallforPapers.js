e20d33224bb7f9d868605cd29596f69a26cb89f2
arunac535 <arunac535@gmail.com>
2023-12-25 17:53:46 +0530

    add

--- client/src/components/CallforPapers.js
+++ client/src/components/CallforPapers.js
@@ -1,12 +1,7 @@
 import React from 'react'
-import Logo from '../components/Logo'
-import Navbar from '../components/Navbar'
 
 export default function CallForPapers() {
   return (
-    <header>
-      <Logo />
-      <Navbar />
-    </header>
+    <div>CallForPapers</div>
   )
 }
