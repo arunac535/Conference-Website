049d58128a67838e10d8486bd6310fc2ba8a63c0
arunac535 <arunac535@gmail.com>
2023-12-26 12:20:54 +0530

    Sponsorship

--- client/src/pages/Sponsorship.js
+++ client/src/pages/Sponsorship.js
@@ -85,54 +85,285 @@ function Sponsorship() {
         <Logo />
         <Navbar />
       </header>
-      <Heading text="Sponsors"/>
-    <Card className="h-full w-500 pt-4  mx-12 shadow-xl">
-      <table className="w-full min-w-max table-auto text-left rounded-lg border">
-        <thead>
-          <tr>
-            {TABLE_HEAD.map((head) => (
-              <th key={head} className="h-16  bg-blue-gray-100 p-4 ">
-                <Typography
-                  variant="small"
-                  color="blue-gray"
-                  className="font-normal leading-none opacity-70"
-                >
-                  {head}
-                </Typography>
+      <div class="flex-grow">
+    
+    
+
+    <div class="my-5">
+      <h1 class="mb-4 text-4xl text-center font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
+        Sponsorship
+      </h1>
+    </div>
+    <section class="py-8 lg:py-16 mx-auto w-full px-4">
+      <div class="text-center">
+        <h2 class="text-4xl lg:text-6xl font-extrabold mb-12 font-helvetica">
+          Rates – Sponsorship and Exhibition
+        </h2>
+      </div>
+    
+      <div class="max-w-7xl mx-auto px-4 overflow-x-auto">
+        <table class="table table-striped table-hover border-separate border-gray-200 rounded-lg shadow-md sm:w-full md:w-full lg:w-full overflow-x-scroll mb-4">
+          <thead class="bg-gradient-to-br from-blue-500 to-blue-700 text-white rounded-t-2xl shadow-lg">
+            <tr class="text-center">
+              <th class="text-2xl font-semibold font-helvetica py-4 pl-4 pr-2" colspan="2">
+                Conference Exhibition Rates(18th Dec. 2024 to 21st Dec. 2024
+                Four days), NITK
               </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {data.map(({ category, fee, entitlement }) => (
-            <tr key={category} className="even:bg-blue-gray-50/50">
-              <td className="p-4">
-                <Typography variant="small" color="blue-gray" className="font-normal">
-                  {category}
-                </Typography>
-              </td>
-              <td className="p-4">
-                <Typography variant="small" color="blue-gray" className="font-normal">
-                  {fee}
-                </Typography>
-              </td>
-              <td className="p-4">
-                <Typography variant="small" color="blue-gray" className="font-normal">
-                  <ul >
-                      {Object.keys(entitlement).map((entKey, entIndex) => (
-                        <li key={entIndex}>{entitlement[entKey]}</li>
-                      ))}
-                    </ul>
-                </Typography>
-              </td>
+              <th class="text-2xl font-semibold font-helvetica px-2 pr-4">
+                Rates in INR
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </Card>
-      <footer>
+          </thead>
+          <tbody>
+            <tr class="bg-gray-100">
+              <td class="text-center font-semibold text-xl sm:text-lg md:text-xl py-4">
+                1.
+              </td>
+              <td class="text-center font-bold py-4">3m x 3m (approx) per stall</td>
+              <td class="text-center font-bold py-4">50000/-</td>
+            </tr>
+            <tr>
+              <td class="text-center font-semibold text-xl sm:text-lg md:text-xl py-4">
+                2.
+              </td>
+              <td class="text-center font-bold py-4">
+                Two stalls of 3m x 3m (premium category)
+              </td>
+              <td class="text-center font-bold py-4">85000/-</td>
+            </tr>
+            <tr class="bg-gray-100">
+              <td class="text-center font-semibold text-xl sm:text-lg md:text-xl py-4">
+                3.
+              </td>
+              <td class="text-center font-bold py-4">
+                More than two stalls – per extra stall of size 3m x 3m
+              </td>
+              <td class="text-center font-bold py-4">25000/-</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
+    
+    <section class="py-8 lg:py-16 mx-auto max-w-screen-xl px-4">
+      <div class="text-center">
+        <h2 class="text-4xl lg:text-6xl font-extrabold mb-12 font-helvetica">
+          Other Sponsorships
+        </h2>
+      </div>
+    
+      <div class="max-w-7xl mx-auto px-4 overflow-x-auto">
+        <table class="table table-striped table-hover border-separate border-gray-200 rounded-lg shadow-md sm:w-full md:w-full lg:w-full overflow-x-scroll mb-4">
+          <thead class="bg-gradient-to-br from-blue-500 to-blue-700 text-white rounded-t-2xl shadow-lg">
+            <tr class="text-center">
+              <th class="text-2xl font-semibold font-helvetica py-4 pl-4 pr-2">
+                SN
+              </th>
+              <th class="text-2xl font-semibold font-helvetica px-2">Item</th>
+              <th class="text-2xl font-semibold font-helvetica px-2">
+                Facilities to the Sponsors
+              </th>
+              <th class="text-2xl font-semibold font-helvetica pr-4">
+                Rates in INR
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="bg-gray-100">
+              <td class="text-center font-semibold text-xl sm:text-lg md:text-xl py-4">
+                1.
+              </td>
+              <td class="text-center font-bold py-4">Food Arrangements</td>
+              <td class="text-center font-semibold py-4">
+                <ol class="sm:pl-0 md:pl-4 text-left py-2">
+                  <li class="mb-2">
+                    1. Advertising at the appropriate places at the food site.
+                  </li>
+                  <li class="mb-2">2. Free food passes for 2 members.</li>
+                  <li class="mb-2">
+                    3. Sponsor logo on tables placed for food at the food site.
+                  </li>
+                  <li class="mb-2">
+                    4. One-page advertisement of sponsor in organizing material kit.
+                  </li>
+                </ol>
+              </td>
+              <td class="text-center font-bold py-4">1.5 Lakhs</td>
+            </tr>
+            <tr>
+              <td class="text-center font-semibold text-xl sm:text-lg md:text-xl py-4">
+                2.
+              </td>
+              <td class="text-center font-bold py-4">Registration Kit</td>
+              <td class="text-center font-semibold py-4">Logo on kit</td>
+              <td class="text-center font-bold py-4">1.5 Lakhs</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
+    
+    <section class="py-8 lg:py-16 mx-auto max-w-screen-xl px-4">
+      <div class="text-center">
+        <h2 class="text-4xl lg:text-6xl font-extrabold mb-12 font-helvetica">
+          Sponsorship Rates
+        </h2>
+      </div>
+    
+      <div class="max-w-7xl mx-auto px-4 overflow-x-auto">
+        <table class="table table-striped table-hover border-separate border-gray-200 rounded-lg shadow-md sm:w-full md:w-full lg:w-full overflow-x-scroll mb-4">
+          <thead class="bg-gradient-to-br from-blue-500 to-blue-700 text-white rounded-t-2xl shadow-lg">
+            <tr class="text-center">
+              <th class="text-2xl font-semibold font-helvetica py-4 pl-4 pr-2 sm:text-lg md:text-xl">
+                SN
+              </th>
+              <th class="text-2xl font-semibold font-helvetica px-2 sm:text-lg md:text-xl">
+                Category
+              </th>
+              <th class="text-2xl font-semibold font-helvetica px-2 sm:text-lg md:text-xl">
+                Max Sponsors
+              </th>
+              <th class="text-2xl font-semibold font-helvetica px-2 sm:text-lg md:text-xl">
+                Facilities
+              </th>
+              <th class="text-2xl font-semibold font-helvetica pr-4 sm:text-lg md:text-xl">
+                Rates in INR
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="bg-gray-100">
+              <td class="text-center font-semibold sm:text-lg md:text-xl py-4">1.</td>
+              <td class="text-center font-bold py-4">Principal Sponsor</td>
+              <td class="text-center font-semibold py-4">1</td>
+              <td class="text-center py-4">
+                <ol class="sm:pl-0 md:pl-4 text-left py-2">
+                  <li class="mb-2 font-semibold">
+                    1. Display of logo at the conference backdrop.
+                  </li>
+                  <li class="mb-2 font-semibold">
+                    2. Display of banners at the conference venue.
+                  </li>
+                  <li class="mb-2 font-semibold">3. 12 free delegates.</li>
+                  <li class="mb-2 font-semibold">
+                    4. Company logo as Principal sponsor on the website.
+                  </li>
+                  <li class="mb-2 font-semibold">
+                    5. Full page colour advertisement in the proceedings.
+                  </li>
+                </ol>
+              </td>
+              <td class="text-center font-bold py-4">10.0 Lakhs</td>
+            </tr>
+            <tr>
+              <td class="text-center font-semibold sm:text-lg md:text-xl py-4">2.</td>
+              <td class="text-center font-bold py-4">Co-Sponsor</td>
+              <td class="text-center font-semibold py-4">1</td>
+              <td class="text-center py-4">
+                <ol class="sm:pl-0 md:pl-4 text-left py-2">
+                  <li class="mb-2 font-semibold">
+                    1. Display of logo at the conference backdrop.
+                  </li>
+                  <li class="mb-2 font-semibold">
+                    2. Display of banners at the conference venue.
+                  </li>
+                  <li class="mb-2 font-semibold">3. 10 free delegates.</li>
+                  <li class="mb-2 font-semibold">
+                    4. Company logo as Co-sponsor on the website.
+                  </li>
+                </ol>
+              </td>
+              <td class="text-center font-bold py-4">8.0 Lakhs</td>
+            </tr>
+            <tr>
+              <td class="text-center font-semibold sm:text-lg md:text-xl py-4">2.</td>
+              <td class="text-center font-bold py-4">Platinum Sponsor</td>
+              <td class="text-center font-semibold py-4">1</td>
+              <td class="text-center py-4">
+                <ol class="sm:pl-0 md:pl-4 text-left py-2">
+                  <li class="mb-2 font-semibold">
+                    1. Display of logo at the conference backdrop.
+                  </li>
+                  <li class="mb-2 font-semibold">
+                    2. Display of banners at the conference venue.
+                  </li>
+                  <li class="mb-2 font-semibold">3. 8 free delegates.</li>
+                  <li class="mb-2 font-semibold">
+                    4. Company logo as Co-sponsor on the website.
+                  </li>
+                </ol>
+              </td>
+              <td class="text-center font-bold py-4">6.0 Lakhs</td>
+            </tr>
+            <tr>
+              <td class="text-center font-semibold sm:text-lg md:text-xl py-4">2.</td>
+              <td class="text-center font-bold py-4">Gold Sponsor</td>
+              <td class="text-center font-semibold py-4">1</td>
+              <td class="text-center py-4">
+                <ol class="sm:pl-0 md:pl-4 text-left py-2">
+                  <li class="mb-2 font-semibold">
+                    1. Display of logo at the conference backdrop.
+                  </li>
+                  <li class="mb-2 font-semibold">
+                    2. Display of banners at the conference venue.
+                  </li>
+                  <li class="mb-2 font-semibold">3. 4 free delegates.</li>
+                  <li class="mb-2 font-semibold">
+                    4. Company logo as Co-sponsor on the website.
+                  </li>
+                </ol>
+              </td>
+              <td class="text-center font-bold py-4">4.0 Lakhs</td>
+            </tr>
+            <tr>
+              <td class="text-center font-semibold sm:text-lg md:text-xl py-4">2.</td>
+              <td class="text-center font-bold py-4">Dinner Sponsor</td>
+              <td class="text-center font-semibold py-4">1</td>
+              <td class="text-center py-4">
+                <ol class="sm:pl-0 md:pl-4 text-left py-2">
+                  <li class="mb-2 font-semibold">
+                    1. Display of logo at the conference backdrop.
+                  </li>
+                  <li class="mb-2 font-semibold">
+                    2. Display of banners at the conference venue.
+                  </li>
+                  <li class="mb-2 font-semibold">3. 4 free delegates.</li>
+                  <li class="mb-2 font-semibold">
+                    4. Company logo as Co-sponsor on the website.
+                  </li>
+                </ol>
+              </td>
+              <td class="text-center font-bold py-4">4.0 Lakhs</td>
+            </tr>
+            <tr>
+              <td class="text-center font-semibold sm:text-lg md:text-xl py-4">2.</td>
+              <td class="text-center font-bold py-4">Lunch Sponsor</td>
+              <td class="text-center font-semibold py-4">1</td>
+              <td class="text-center py-4">
+                <ol class="sm:pl-0 md:pl-4 text-left py-2">
+                  <li class="mb-2 font-semibold">
+                    1. Display of logo at the conference backdrop.
+                  </li>
+                  <li class="mb-2 font-semibold">
+                    2. Display of banners at the conference venue.
+                  </li>
+                  <li class="mb-2 font-semibold">3. 4 free delegates.</li>
+                  <li class="mb-2 font-semibold">
+                    4. Company logo as Co-sponsor on the website.
+                  </li>
+                </ol>
+              </td>
+              <td class="text-center font-bold py-4">4.0 Lakhs</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
+    <footer>
         <Footer />
       </footer>
+    
+    </div>
     </>
   );
 }
