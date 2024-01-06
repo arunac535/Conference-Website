ff4d20ec586af23ce3dff93a28206ff0f50e6839
Aditya <adityachauhan2501@gmail.com>
2024-01-07 01:38:14 +0530

    regis-table-updated

--- client/src/pages/Registration.js
+++ client/src/pages/Registration.js
@@ -7,37 +7,39 @@ import Heading from "../components/Heading";
 import RegistrationTimeline from "../components/Timeline/RegistrationTimeline";
 import Footer from "../components/Footer";
 
-const TABLE_HEAD=[
+const TABLE_HEAD = [
   "Authors/Registration",
-  "UG students",
-  "M.tech / Ph.D students Faculties (Max. 04)",
-  "Others",
-  "Listeners"
-]
+  "Offline(in INR)",
+  "Online(in INR)",
+  "Foreign Authors(in USD)",
+];
 
-const data=[
+const data = [
   {
-    category: "Memebers (in INR)",
-    UG: "5000",
-    MTech: "5500",
-    Others: "6000",
-    Listeners:"2000"
+    category: "UG Students",
+    offline: "3000",
+    online: "5500",
+    foreign: "249",
   },
   {
-    category: "Non-Memebers (in INR)",
-    UG: "5500",
-    MTech: "6000",
-    Others: "6500",
-    Listeners:"2500"
+    category: "M.Tech/PHD/Faculties(max 4)",
+    offline: "5500",
+    online: "6000",
+    foreign: "299",
   },
   {
-    category: "Foreign Authors (in USD)",
-    UG: "249",
-    MTech: "299",
-    Others: "329",
-    Listeners:"119"
-  },  
-]
+    category: "Others",
+    offline: "6500",
+    online: "7000",
+    foreign: "329",
+  },
+  {
+    category: "Listeners",
+    offline: "2000",
+    online: "2500",
+    foreign: "199",
+  },
+];
 
 function Registration() {
   return (
@@ -52,84 +54,55 @@ function Registration() {
 
         <RegistrationTimeline />
 
-        <div className="mx-5 mt-20">
-          <Card className="shadow-xl">
-            <table className="text-left rounded-lg border table-auto">
-              <thead>
-                <tr>
-                  {TABLE_HEAD.map((head) => (
-                    <th
-                      key={head}
-                      className="text-sm sm:text-md font-bold bg-blue-gray-100 p-4"
+        <Card className="shadow-xl overflow-x-auto mt-10 mb-20 w-[90%] sm:w-[75%] md:w-[60%]">
+          <table className="text-left rounded-lg border">
+            <thead>
+              <tr>
+                {TABLE_HEAD.map((head) => (
+                  <th key={head} className="bg-blue-gray-100 p-4">
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-extrabold leading-none opacity-100"
                     >
-                      <Typography
-                        variant="small"
-                        color="blue-gray"
-                        className="font-extrabold leading-none opacity-100"
-                      >
-                        {head}
-                      </Typography>
-                    </th>
-                  ))}
+                      {head}
+                    </Typography>
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {data.map(({ category, offline, online, foreign }) => (
+                <tr key={category} className="even:bg-blue-gray-50/50">
+                  <td className="p-2">
+                    <Typography
+                      // variant="small"
+                      color="blue-gray"
+                      className="text-sm font-bold"
+                    >
+                      {category}
+                    </Typography>
+                  </td>
+                  <td className="p-4">
+                    <Typography variant="small" color="blue-gray">
+                      {offline}
+                    </Typography>
+                  </td>
+                  <td className="p-4">
+                    <Typography variant="small" color="blue-gray">
+                      {online}
+                    </Typography>
+                  </td>
+                  <td className="p-4">
+                    <Typography variant="small" color="blue-gray">
+                      {foreign}
+                    </Typography>
+                  </td>
                 </tr>
-              </thead>
-              <tbody>
-                {data.map(
-                  ({ category,UG,MTech,Others,Listeners,SIJ}) => (
-                    <tr key={category} className="even:bg-blue-gray-50/50">
-                      <td className="p-2 sm:p-4">
-                        <Typography
-                          variant="small"
-                          color="blue-gray"
-                          className="font-bold text-center" 
-                        >
-                          {category}
-                        </Typography>
-                      </td>
-                      <td className="p-2 sm:p-4">
-                        <Typography
-                          variant="small"
-                          color="blue-gray"
-                          className="font-normal text-center"
-                        >
-                          {UG}
-                        </Typography>
-                      </td>
-                      <td className="p-2 sm:p-4">
-                        <Typography
-                          variant="small"
-                          color="blue-gray"
-                          className="font-normal text-center"
-                        >
-                          {MTech}
-                        </Typography>
-                      </td>
-                      <td className="p-2 sm:p-4">
-                        <Typography
-                          variant="small"
-                          color="blue-gray"
-                          className="font-normal text-center"
-                        >
-                          {Others}
-                        </Typography>
-                      </td>
-                      <td className="p-2 sm:p-4">
-                        <Typography
-                          variant="small"
-                          color="blue-gray"
-                          className="font-normal text-center"
-                        >
-                          {Listeners}
-                        </Typography>
-                      </td>
-
-                    </tr>
-                  )
-                )}
-              </tbody>
-            </table>
-          </Card>
-        </div>
+              ))}
+            </tbody>
+          </table>
+        </Card>
       </main>
       <footer>
         <Footer />
