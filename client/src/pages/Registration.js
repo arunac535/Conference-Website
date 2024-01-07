258bc1782dcf61319f93cf51eec18193c925e3ea
arunac535 <arunac535@gmail.com>
2024-01-07 11:25:44 +0530

    regtable

--- client/src/pages/Registration.js
+++ client/src/pages/Registration.js
@@ -7,14 +7,15 @@ import Heading from "../components/Heading";
 import RegistrationTimeline from "../components/Timeline/RegistrationTimeline";
 import Footer from "../components/Footer";
 
-const TABLE_HEAD = [
+const TABLE_HEAD=[
   "Authors/Registration",
-  "Offline(in INR)",
-  "Online(in INR)",
-  "Foreign Authors(in USD)",
-];
+  "UG students",
+  "M.tech / Ph.D students Faculties (Max. 04)",
+  "Others",
+  "Listeners"
+]
 
-const data = [
+const data=[
   {
     category: "Memebers (in INR)",
     UG: "3000",
@@ -51,55 +52,84 @@ function Registration() {
 
         <RegistrationTimeline />
 
-        <Card className="shadow-xl overflow-x-auto mt-10 mb-20 w-[90%] sm:w-[75%] md:w-[60%]">
-          <table className="text-left rounded-lg border">
-            <thead>
-              <tr>
-                {TABLE_HEAD.map((head) => (
-                  <th key={head} className="bg-blue-gray-100 p-4">
-                    <Typography
-                      variant="small"
-                      color="blue-gray"
-                      className="font-extrabold leading-none opacity-100"
+        <div className="mx-5 mt-20">
+          <Card className="shadow-xl">
+            <table className="text-left rounded-lg border table-auto">
+              <thead>
+                <tr>
+                  {TABLE_HEAD.map((head) => (
+                    <th
+                      key={head}
+                      className="text-sm sm:text-md font-bold bg-blue-gray-100 p-4"
                     >
-                      {head}
-                    </Typography>
-                  </th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              {data.map(({ category, offline, online, foreign }) => (
-                <tr key={category} className="even:bg-blue-gray-50/50">
-                  <td className="p-2">
-                    <Typography
-                      // variant="small"
-                      color="blue-gray"
-                      className="text-sm font-bold"
-                    >
-                      {category}
-                    </Typography>
-                  </td>
-                  <td className="p-4">
-                    <Typography variant="small" color="blue-gray">
-                      {offline}
-                    </Typography>
-                  </td>
-                  <td className="p-4">
-                    <Typography variant="small" color="blue-gray">
-                      {online}
-                    </Typography>
-                  </td>
-                  <td className="p-4">
-                    <Typography variant="small" color="blue-gray">
-                      {foreign}
-                    </Typography>
-                  </td>
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="font-extrabold leading-none opacity-100"
+                      >
+                        {head}
+                      </Typography>
+                    </th>
+                  ))}
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </Card>
+              </thead>
+              <tbody>
+                {data.map(
+                  ({ category,UG,MTech,Others,Listeners,SIJ}) => (
+                    <tr key={category} className="even:bg-blue-gray-50/50">
+                      <td className="p-2 sm:p-4">
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-bold text-center" 
+                        >
+                          {category}
+                        </Typography>
+                      </td>
+                      <td className="p-2 sm:p-4">
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal text-center"
+                        >
+                          {UG}
+                        </Typography>
+                      </td>
+                      <td className="p-2 sm:p-4">
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal text-center"
+                        >
+                          {MTech}
+                        </Typography>
+                      </td>
+                      <td className="p-2 sm:p-4">
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal text-center"
+                        >
+                          {Others}
+                        </Typography>
+                      </td>
+                      <td className="p-2 sm:p-4">
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal text-center"
+                        >
+                          {Listeners}
+                        </Typography>
+                      </td>
+
+                    </tr>
+                  )
+                )}
+              </tbody>
+            </table>
+          </Card>
+        </div>
       </main>
       <footer>
         <Footer />
