a12b312693fe61a807601e7a32b87b37d1a4d2e9
Aditya <adityachauhan2501@gmail.com>
2024-03-07 21:55:56 +0530

    updated-speakers

--- client/src/components/SpeakerCard.js
+++ client/src/components/SpeakerCard.js
@@ -1,17 +1,16 @@
 import React from "react";
 
-function SpeakerCard({ name, afiliation, path }) {
+function SpeakerCard({ ind, name, afiliation, path }) {
   return (
-    <div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 my-10 py-10">
-        <div className="bg-gray-100 py-8 px-4 rounded-lg shadow-xl">
-          <img src={path} height={180} width={205} />
-          <p className="text-base uppercase font-bold text-gray-800">{name}</p>
-          <p className="text-xs pt-2 font-semibold text-gray-600 uppercase">
-            {afiliation}
-          </p>
-        </div>
-      </div>
+    <div
+      className="bg-gray-100 py-8 px-4 rounded-lg shadow-xl flex flex-col justify-center items-center"
+      key={ind}
+    >
+      <img src={path} height={180} width={205} />
+      <p className="font-extrabold uppercase text-gray-800 py-3">{name}</p>
+      <p className="text-xs pt-2 font-semibold text-gray-600 uppercase">
+        {afiliation}
+      </p>
     </div>
   );
 }
